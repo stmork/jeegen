1676683a5992c355ec8fdb2d7313bdf1dc2d0732
Steffen A. Mork <linux-dev@morknet.de>
2012-06-06 11:48:10 +0000

    * Java Validierung eingebaut.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@50 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6/src/de/itemis/jee6/validation/DslJavaValidator.java
+++ JEE6/de.itemis.jee6/src/de/itemis/jee6/validation/DslJavaValidator.java
@@ -1,13 +1,113 @@
 package de.itemis.jee6.validation;
+
+import java.util.List;
+
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.validation.Check;
+
+import de.itemis.jee6.jee6.Entity;
+import de.itemis.jee6.jee6.History;
+import de.itemis.jee6.jee6.Jee6Package;
+import de.itemis.jee6.jee6.Model;
+import de.itemis.jee6.jee6.Text;
+import de.itemis.jee6.jee6.Timestamp;
  
 
-public class DslJavaValidator extends AbstractDslJavaValidator {
+public class DslJavaValidator extends AbstractDslJavaValidator
+{
+	@Check
+	public void checkContextPath(Model model)
+	{
+		if (!model.getPath().startsWith("/"))
+		{
+			error("Context path does not start wit '/'!", Jee6Package.Literals.MODEL__PATH);
+		}
+	}
+	
+	@Check
+	public void checkHistory(Entity entity)
+	{
+		List<History> histories = EcoreUtil2.typeSelect(entity.getTypes(), History.class);
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+		if (histories.size() > 1)
+		{
+			for(History history : histories)
+			{
+				error ("Es darf nur max. eine Historie vorhanden sein!", history, Jee6Package.Literals.HISTORY__TYPE, 0);
+			}
+		}
+	}
 
+	@Check
+	public void checkText(Text text)
+	{
+		guard(text.isId());
+		if (text.isId())
+		{
+			final Entity entity = (Entity)text.eContainer();
+			int idCount = 0;
+	
+			for (Text t : EcoreUtil2.typeSelect(entity.getTypes(), Text.class))
+			{
+				if (t.isId())
+				{
+					idCount++;
+				}
+			}
+	
+			if (idCount > 1)
+			{
+				error("Es darf nur max. ein Attribut eine ID sein!",  Jee6Package.Literals.TEXT__ID);
+			}
+		}
+	}
+
+	@Check
+	public void checkTimestampAuto(Timestamp timestamp)
+	{
+		int prePersistCount = 0;
+
+		if (timestamp.isAuto())
+		{
+			final Entity entity = (Entity)timestamp.eContainer();
+	
+			for (Timestamp t : EcoreUtil2.typeSelect(entity.getTypes(), Timestamp.class))
+			{
+				if (t.isAuto())
+				{
+					prePersistCount++;
+				}
+			}
+		}
+
+		if (prePersistCount > 1)
+		{
+			warning("Es macht nur eine automatische Datumsmarkierung Sinn!", Jee6Package.Literals.TIMESTAMP__AUTO);
+		}
+	}
+	
+
+	@Check
+	public void checkTimestampUpdate(Timestamp timestamp)
+	{
+		int preUpdateCount = 0;
+
+		if (timestamp.isUpdate())
+		{
+			final Entity entity = (Entity)timestamp.eContainer();
+	
+			for (Timestamp t : EcoreUtil2.typeSelect(entity.getTypes(), Timestamp.class))
+			{
+				if (t.isUpdate())
+				{
+					preUpdateCount++;
+				}
+			}
+		}
+
+		if (preUpdateCount > 1)
+		{
+			warning("Es macht nur eine automatische Datumsaktualisierung Sinn!", Jee6Package.Literals.TIMESTAMP__UPDATE);
+		}
+	}
 }
