7e153e4609d17a18f46d0a24917f9f0ea31fdec8
Steffen A. Mork <linux-dev@morknet.de>
2012-06-07 12:44:02 +0000

    * DAOs aufgebohrt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@55 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/FacesTest/src/de/itemis/faces/dao/AdminDaoBean.java
+++ JEE6/FacesTest/src/de/itemis/faces/dao/AdminDaoBean.java
@@ -4,21 +4,9 @@ import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 
 import de.itemis.faces.Profiler;
-import de.itemis.faces.entities.AddressOption;
 
 @Stateless
 @Interceptors(Profiler.class)
 public class AdminDaoBean extends AbstractAdminDaoBean
 {
-	public AddressOption ensure(final AddressOption.AddressOptionEnum type, final String description)
-	{
-		AddressOption option = em.find(AddressOption.class, type.ordinal());
-		
-		if (option == null)
-		{
-			option = new AddressOption(type, description);
-			em.persist(option);
-		}
-		return option;
-	}
 }
