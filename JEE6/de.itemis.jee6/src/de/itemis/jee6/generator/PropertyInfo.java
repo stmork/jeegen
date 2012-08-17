f6691d9aef248317b9081841ff053363e3bec557
Steffen A. Mork <linux-dev@morknet.de>
2012-08-17 11:53:09 +0000

    * Properties werden jetzt sortiert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@107 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6/src/de/itemis/jee6/generator/PropertyInfo.java
+++ JEE6/de.itemis.jee6/src/de/itemis/jee6/generator/PropertyInfo.java
@@ -6,68 +6,70 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 
-class PropertyInfo extends Properties
-{
+class PropertyInfo extends Properties {
 	private static final long serialVersionUID = 1L;
 	private URL url;
 	private boolean modified = false;
 	private Log log;
-	
-	PropertyInfo(final Log log, final String filename) throws IOException
-	{
+
+	PropertyInfo(final Log log, final String filename) throws IOException {
 		this.log = log;
 		InputStream is = null;
-		try
-		{
-			url = Thread.currentThread ().getContextClassLoader ().getResource(filename);
+		try {
+			url = Thread.currentThread().getContextClassLoader()
+					.getResource(filename);
 
-			if (url != null)
-			{
+			if (url != null) {
 				is = url.openStream();
 				log.info("Reading " + url);
 				load(is);
 			}
-		}
-		finally
-		{
-			if (is != null)
-			{
+		} finally {
+			if (is != null) {
 				is.close();
 			}
 		}
 	}
-	
-	void set(final String key, final String value)
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public synchronized Enumeration keys()
 	{
-		if (get(key) == null)
+		Enumeration keysEnum = super.keys();
+		Vector keyList = new Vector();
+		while (keysEnum.hasMoreElements())
 		{
-			log.info(String.format(" * Adding %s=%s", key,value));
+			keyList.add(keysEnum.nextElement());
+		}
+		Collections.sort(keyList);
+		return keyList.elements();
+	}
+
+	void set(final String key, final String value) {
+		if (get(key) == null) {
+			log.info(String.format(" * Adding %s=%s", key, value));
 			setProperty(key, value);
 			modified = true;
 		}
 	}
 
-	void flush() throws IOException, URISyntaxException
-	{
-		if (modified && (url != null))
-		{
+	void flush() throws IOException, URISyntaxException {
+		if (modified && (url != null)) {
 			OutputStream os = null;
-			
-			try
-			{
+
+			try {
 				log.info("Storing updated " + url.getFile());
 
 				os = new FileOutputStream(url.getFile());
 				store(os, "Automatically extended");
-			}
-			finally
-			{
-				if (os != null)
-				{
+			} finally {
+				if (os != null) {
 					os.close();
 				}
 			}
