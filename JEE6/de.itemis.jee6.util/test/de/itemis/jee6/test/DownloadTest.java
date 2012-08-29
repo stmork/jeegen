554a42eeafadb8a0e3b7b6908d70c19dcc3d4b26
Steffen A. Mork <linux-dev@morknet.de>
2012-08-29 06:49:04 +0000

    * Download Test erweitert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@134 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6.util/test/de/itemis/jee6/test/DownloadTest.java
+++ JEE6/de.itemis.jee6.util/test/de/itemis/jee6/test/DownloadTest.java
@@ -1,26 +1,53 @@
 package de.itemis.jee6.test;
 
+import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 import de.itemis.jee6.util.Download;
 
-public class DownloadTest {
+public class DownloadTest
+{
+	private final static String IMAGE_URL = "http://www.itemis.de/binary.ashx/element=E0E0E0/~image.attribute/97/image.gif";
+
 	@Test
 	public void itemis() throws IOException
 	{
 		final Download download = new Download("http://www.itemis.de");
 		
-		download.downloadArray();
+		final byte [] array = download.downloadArray();
+		Assert.assertNotNull(array);
+	}
+
+	@Test
+	public void favicon() throws IOException
+	{
+		final Download download = new Download(IMAGE_URL);
+		
+		final byte [] array = download.downloadArray();
+		Assert.assertNotNull(array);
+		
+		final BufferedImage image = Download.read(array);
+		Assert.assertNotNull(image);
+	}
+
+	@Test
+	public void image() throws IOException
+	{
+		final Download download = new Download(IMAGE_URL);
+		final BufferedImage image = download.downloadImage();
+
+		Assert.assertNotNull(image);
 	}
 
 	@Test(expected=FileNotFoundException.class)
 	public void error() throws IOException
 	{
 		final Download download = new Download("http://www.itemis.de/mork");
-		
 		download.downloadArray();
 	}
 }
