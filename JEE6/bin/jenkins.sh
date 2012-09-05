8865c3530266172319e523c67a48303867b9640d
Steffen A. Mork <linux-dev@morknet.de>
2012-09-05 05:13:29 +0000

    * Ant Aufrufe verkürzt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@147 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/bin/jenkins.sh
+++ JEE6/bin/jenkins.sh
@@ -3,33 +3,19 @@
 set -e
 
 cd de.itemis.jee6.util
-ant clean
-ant package
-ant javadoc
-ant emma
+ant clean package javadoc emma
 
 cd ../de.itemis.jee6
-ant clean
-ant generate
-ant deploy
+ant clean generate deploy
 
 cd ../de.itemis.jee6.ui
-ant clean
-ant deploy
+ant clean deploy
 
 cd ../MinimalWebApplication
-ant clean
-ant package
+ant clean package
 
 cd ../FacesGenerated
-ant clobber
-ant generate
-ant package
-ant findbugs
+ant clobber generate package findbugs
 
 cd ../FacesTest
-ant clean
-ant generate
-ant package
-ant findbugs
-ant test
+ant clean generate package findbugs test
