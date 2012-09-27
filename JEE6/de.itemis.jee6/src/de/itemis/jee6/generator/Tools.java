c70f5d227762ea0914ad201e1ca78f64005ee597
Steffen A. Mork <linux-dev@morknet.de>
2012-09-27 06:54:44 +0000

    * Beautifier aufgeräumt.  * Dependencies aktualisiert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@192 2b804697-b555-4dd3-bbc5-4dc99f919620

---
+++ JEE6/de.itemis.jee6/src/de/itemis/jee6/generator/Tools.java
@@ -0,0 +1,12 @@
+package de.itemis.jee6.generator;
+
+import java.util.Calendar;
+
+public class Tools {
+	public static int year()
+	{
+		Calendar cal = Calendar.getInstance();
+		
+		return cal.get(Calendar.YEAR);
+	}
+}
