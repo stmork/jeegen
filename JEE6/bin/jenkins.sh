38346f8fbf86d58d2eff1c4a67d9e920e3f19c75
Steffen A. Mork <linux-dev@morknet.de>
2012-10-05 14:14:03 +0000

    * Regenerate DSL funktioniert jetzt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@202 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/bin/jenkins.sh
+++ JEE6/bin/jenkins.sh
@@ -6,8 +6,8 @@ cd de.itemis.jee6.util
 ant clean package javadoc emma
 
 cd ../de.itemis.jee6
-ant clean generate
-ant deploy
+ant undeploy
+ant clean generate deploy
 
 cd ../de.itemis.jee6.ui
 ant clean deploy
