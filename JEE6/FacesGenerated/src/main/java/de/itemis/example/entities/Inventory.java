6be825b0dba1f89bf955d09439b19af2d894aff8
Steffen A. Mork <linux-dev@morknet.de>
2012-10-16 17:58:32 +0000

    * 1:n Entitites - Fortsetzung.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@219 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/FacesGenerated/src/main/java/de/itemis/example/entities/Inventory.java
+++ JEE6/FacesGenerated/src/main/java/de/itemis/example/entities/Inventory.java
@@ -1,8 +1,11 @@
 package de.itemis.example.entities;
 
-import javax.persistence.*;
-
 import java.util.Locale;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
