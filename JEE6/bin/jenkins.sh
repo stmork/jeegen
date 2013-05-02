978b914f0e31f127e9be77ff174254c7ce322200
Steffen A. Mork <linux-dev@morknet.de>
2013-05-02 13:16:34 +0000

    * Suche nach JDK6 ausgebaut.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@275 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/bin/jenkins.sh
+++ JEE6/bin/jenkins.sh
@@ -4,9 +4,11 @@ set -e
 
 cd `dirname $0`
 
-JVM_PATH=/usr/lib/jvm/java-6-openjdk-amd64
+OPEN_JDK=/usr/lib/jvm/java-6-openjdk-amd64
+ORACLE_JDK=/usr/lib/jvm/java-6-oracle
 
-test -d $JVM_PATH && export JAVA_HOME=$JVM_PATH
+test -d $OPEN_JDK   && export JAVA_HOME=$OPEN_JDK
+test -d $ORACLE_JDK && export JAVA_HOME=$ORACLE_JDK
 
 cd ../de.itemis.jee6.util
 ant clean package javadoc jacoco
