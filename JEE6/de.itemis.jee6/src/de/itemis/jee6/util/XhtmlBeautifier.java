f91b92f8cd674df28eb3a0eabfd52051de0e2aa3
Steffen A. Mork <linux-dev@morknet.de>
2013-06-19 13:03:02 +0000

    * HTML-Beautifier fertig.  * XHTML wird HTML5-konform generiert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@297 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6/src/de/itemis/jee6/util/XhtmlBeautifier.java
+++ JEE6/de.itemis.jee6/src/de/itemis/jee6/util/XhtmlBeautifier.java
@@ -5,34 +5,85 @@ import java.io.StringWriter;
 import java.io.Writer;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.xpand2.output.FileHandle;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-
-import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 import de.morknet.oaw.tools.postprocessor.XmlBeautifier;
 
 public class XhtmlBeautifier extends XmlBeautifier
 {
+	private final static Log log = LogFactory.getLog(XhtmlBeautifier.class);
+
 	@Override
-	protected String formatXml(final Document document) throws IOException, ParserConfigurationException, SAXException
+	public void beforeWriteAndClose(FileHandle handle)
 	{
-		final OutputFormat format = new OutputFormat(document);
-		final Writer out = new StringWriter();
-		
-		format.setMethod("html");
-		format.setDoctype("http", "html");
-		format.setEncoding("UTF-8");
-		format.setLineWidth(120);
-		format.setIndenting(true);
-		format.setIndent(4);
-		format.setOmitXMLDeclaration(true);
-		format.setOmitDocumentType(false);
-		final XMLSerializer serializer = new XMLSerializer(out, format);
-		serializer.serialize(document);
-		
-		return out.toString();
+		final String filename = handle.getAbsolutePath(); 
+
+		if (isXmlFile(filename))
+		{
+			try
+			{
+				final String unformattedXml = handle.getBuffer().toString().trim(); 
+
+				handle.setBuffer(prettyPrintXml(unformattedXml, filename.endsWith("/layout.xhtml")));
+			}
+			catch (Exception e)
+			{
+				log.error(e);
+			}
+		}
+	}
+
+	protected String prettyPrintXml(
+			final String  unformattedXml,
+			final boolean isLayout) throws ParserConfigurationException, SAXException, IOException
+	{
+        final Document document = parseXmlFile(unformattedXml);
+		Writer out = null;
+		String result = null;
+
+		try
+		{		
+			// Create an "identity" transformer - copies input to output
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+	
+			// For "HTML" serialization, use
+			t.setOutputProperty(OutputKeys.METHOD,               "xml");
+			t.setOutputProperty(OutputKeys.INDENT,               "4");
+			t.setOutputProperty(OutputKeys.ENCODING,             "UTF-8");
+			t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			t.setOutputProperty(OutputKeys.VERSION,              "5.0");
+			if (isLayout)
+			{
+				t.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "html");
+//				t.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "html");
+			}
+	
+			// Serialize DOM tree
+			out = new StringWriter();
+			t.transform(new DOMSource(document), new StreamResult(out));
+			result = out.toString();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (out != null)
+			{
+				out.close();
+			}
+		}
+		return result;
 	}
 }
