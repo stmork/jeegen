de58baa75b0033e14c36df2ee57e64d4e1351858
Steffen A. Mork <linux-dev@morknet.de>
2014-01-09 06:03:52 +0000

    * Download Test von www.itemis.de wegkonfiguriert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@354 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6.util/src/test/java/de/itemis/jee6/test/DownloadTest.java
+++ JEE6/de.itemis.jee6.util/src/test/java/de/itemis/jee6/test/DownloadTest.java
@@ -12,16 +12,41 @@ import de.itemis.jee6.util.Download;
 
 public class DownloadTest
 {
-	private final static String IMAGE_URL = "http://www.itemis.de/binary.ashx/element=E0E0E0/~image.attribute/97/image.gif";
+	private final static String HOMEPAGE_URL = "http://eisenbahnsteuerung.org";
+	private final static String IMAGE_URL    = HOMEPAGE_URL + "/images/rcc32.gif";
+
+	@Test
+	public void eisenbahnsteuerung() throws IOException
+	{
+		final Download download = new Download(HOMEPAGE_URL);
+
+		final byte [] array = download.downloadArray();
+		Assert.assertNotNull(array);
+
+		final String mimeType = download.getMimeType();
+		Assert.assertTrue(mimeType.startsWith("text/html"));
+	}
+
+	@Test
+	public void morknet() throws IOException
+	{
+		final Download download = new Download("http://morknet.de");
+
+		final byte [] array = download.downloadArray();
+		Assert.assertNotNull(array);
+
+		final String mimeType = download.getMimeType();
+		Assert.assertTrue(mimeType.startsWith("text/html"));
+	}
 
 	@Test
 	public void itemis() throws IOException
 	{
-		final Download download = new Download("http://www.itemis.de");
-		
+		final Download download = new Download("http://jee6-generator.itemis.de");
+
 		final byte [] array = download.downloadArray();
 		Assert.assertNotNull(array);
-		
+
 		final String mimeType = download.getMimeType();
 		Assert.assertTrue(mimeType.startsWith("text/html"));
 	}
@@ -33,16 +58,16 @@ public class DownloadTest
 
 		String mimeType = download.getMimeType();
 		Assert.assertNull(mimeType);
-		
+
 		final byte [] array = download.downloadArray();
 		Assert.assertNotNull(array);
-		
+
 		mimeType = download.getMimeType();
 		Assert.assertTrue(mimeType.startsWith("image/gif"));
-		
+
 		BufferedImage image = Download.parse(array);
 		Assert.assertNotNull(image);
-		
+
 		mimeType = download.getMimeType();
 		Assert.assertTrue(mimeType.startsWith("image/gif"));
 
@@ -56,13 +81,13 @@ public class DownloadTest
 	@Test(expected=FileNotFoundException.class)
 	public void error404() throws IOException
 	{
-		final Download download = new Download("http://www.itemis.de/mork");
+		final Download download = new Download("http://morknet.de/404");
 		download.downloadArray();
 	}
 
 	@Test(expected=MalformedURLException.class)
 	public void malformedUrl() throws IOException
 	{
-		new Download("itemis");
+		new Download("morknet");
 	}
 }
