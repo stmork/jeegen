01b6890df2967a4507a1f95943042aa4f16c7409
Steffen A. Mork <linux-dev@morknet.de>
2014-04-07 13:24:43 +0000

    * Deploy funktioniert jetzt. :-)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@410 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6.website/deploy.sh
+++ JEE6/de.itemis.jee6.website/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-scp -r ${PWD}/website/* root@updates.itemis.de:/data/jee6-generator/
+TMP_WEBSITE=/tmp/website
+
+rsync -av --delete ${PWD}/website/ ${TMP_WEBSITE}/
+find  ${TMP_WEBSITE} -type d -name .svn | xargs rm -rf
+chmod -R g+rX,o+rX ${TMP_WEBSITE}/
+
+rsync -av --delete ${TMP_WEBSITE}/ root@updates.itemis.de:/data/jee6-generator/
 
 echo "Website deployed"
