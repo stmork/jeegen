5567744ad99b17d37e9bb75bfe6f193327a34d85
Dominik Pieper <github@pieper.io>
2014-06-13 14:00:01 +0000

    * Wizards für Ant und MAven hinzugefügt
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@534 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE6/de.itemis.jee6.ui/src/de/itemis/jee6/ui/wizard/DslProjectInfo.java
+++ JEE6/de.itemis.jee6.ui/src/de/itemis/jee6/ui/wizard/DslProjectInfo.java
@@ -3,6 +3,9 @@ package de.itemis.jee6.ui.wizard;
 import org.eclipse.xtext.ui.wizard.DefaultProjectInfo;
 
 public class DslProjectInfo extends DefaultProjectInfo {
+
+	private boolean isMavenProject;
+
 	public String getApplicationName()
 	{
 		return getProjectName().replaceAll(" ", "").toLowerCase();
@@ -11,5 +14,13 @@ public class DslProjectInfo extends DefaultProjectInfo {
 	public String getBasePackage()
 	{
 		return "org.example.jee6";
+	}
+
+	public boolean isMavenProject() {
+		return isMavenProject;
+	}
+
+	public void setMavenProject(boolean isMavenProject) {
+		this.isMavenProject = isMavenProject;
 	}
 }
