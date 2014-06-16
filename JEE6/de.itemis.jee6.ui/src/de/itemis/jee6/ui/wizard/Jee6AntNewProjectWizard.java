8c715a2c66263ccf5fcdf1aa56ead4e0538cd553
Dominik Pieper <github@pieper.io>
2014-06-16 06:58:40 +0000

    * Wizards hinzugefügt
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@554 2b804697-b555-4dd3-bbc5-4dc99f919620

---
+++ JEE6/de.itemis.jee6.ui/src/de/itemis/jee6/ui/wizard/Jee6AntNewProjectWizard.java
@@ -0,0 +1,42 @@
+package de.itemis.jee6.ui.wizard;
+
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.xtext.ui.wizard.IProjectCreator;
+import org.eclipse.xtext.ui.wizard.IProjectInfo;
+import org.eclipse.xtext.ui.wizard.XtextNewProjectWizard;
+
+import com.google.inject.Inject;
+
+public class Jee6AntNewProjectWizard extends XtextNewProjectWizard {
+
+	private WizardNewProjectCreationPage mainPage;
+	private String title = "Ant based JEE6 Generator Project";
+
+	@Inject
+	public Jee6AntNewProjectWizard(IProjectCreator projectCreator) {
+		super(projectCreator);
+		setWindowTitle(title);
+	}
+
+	/**
+	 * Use this method to add pages to the wizard.
+	 * The one-time generated version of this class will add a default new project page to the wizard.
+	 */
+	public void addPages() {
+		mainPage = new WizardNewProjectCreationPage("basicNewProjectPage");
+		mainPage.setTitle(title);
+		mainPage.setDescription("Create a new JEE6 Generator project.");
+		addPage(mainPage);
+	}
+
+	/**
+	 * Use this method to read the project settings from the wizard pages and feed them into the project info class.
+	 */
+	@Override
+	protected IProjectInfo getProjectInfo() {
+		de.itemis.jee6.ui.wizard.DslProjectInfo projectInfo = new de.itemis.jee6.ui.wizard.DslProjectInfo();
+		projectInfo.setProjectName(mainPage.getProjectName());
+		projectInfo.setMavenProject(false);
+		return projectInfo;
+	}
+}
