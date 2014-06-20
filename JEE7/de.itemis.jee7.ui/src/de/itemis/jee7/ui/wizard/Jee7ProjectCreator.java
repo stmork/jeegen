d9d68e16130c8fbbffe4cba258b85c09c3f8b0d2
Dominik Pieper <github@pieper.io>
2014-06-20 13:39:14 +0000

    * fix utils version
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@568 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE7/de.itemis.jee7.ui/src/de/itemis/jee7/ui/wizard/Jee7ProjectCreator.java
+++ JEE7/de.itemis.jee7.ui/src/de/itemis/jee7/ui/wizard/Jee7ProjectCreator.java
@@ -52,7 +52,7 @@ public class Jee7ProjectCreator extends DslProjectCreator
 			copyFile(project, "resources/logo.png",       RES_ROOT + "/img");
 			copyFile(project, "resources/favicon.ico",    RES_ROOT + "/img");
 			if(!getProjectInfo().isMavenProject()) {
-				copyFile(project, "resources/jee6-utils.jar", WEB_CONTENT_ROOT + "/WEB-INF/lib");
+				copyFile(project, "resources/jee7-utils.jar", WEB_CONTENT_ROOT + "/WEB-INF/lib");
 			}
 		}
 		catch(Exception e)
