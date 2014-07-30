cd664c22ae255943dbd71aa827cb87047d56a32b
Steffen A. Mork <linux-dev@morknet.de>
2014-07-30 07:20:52 +0000

    * Formatting.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@577 2b804697-b555-4dd3-bbc5-4dc99f919620

--- JEE7/de.itemis.jee7/src/de/itemis/jee7/DslRuntimeModule.java
+++ JEE7/de.itemis.jee7/src/de/itemis/jee7/DslRuntimeModule.java
@@ -10,7 +10,8 @@ import de.itemis.jee7.formatting.Jee7LinkingDiagnosticMessageProvider;
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
-public class DslRuntimeModule extends de.itemis.jee7.AbstractDslRuntimeModule {
+public class DslRuntimeModule extends de.itemis.jee7.AbstractDslRuntimeModule
+{
 	public Class<? extends ILinkingDiagnosticMessageProvider> bindILinkingDiagnosticMessageProvider()
 	{
 		return Jee7LinkingDiagnosticMessageProvider.class;
