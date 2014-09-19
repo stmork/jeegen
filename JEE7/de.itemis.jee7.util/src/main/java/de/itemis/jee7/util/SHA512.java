184f645616d1f8eb4fd9a7f8a240dcf51e22cfb8
Steffen A. Mork <linux-dev@morknet.de>
2014-09-19 11:57:22 +0000

    * Merge von JEE6 nach JEE7.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@589 2b804697-b555-4dd3-bbc5-4dc99f919620

---
+++ JEE7/de.itemis.jee7.util/src/main/java/de/itemis/jee7/util/SHA512.java
@@ -0,0 +1,16 @@
+package de.itemis.jee7.util;
+
+
+/**
+ * This class provides support methods for computing an SHA-512 hash.
+ */
+public class SHA512 extends AbstractMessageDigest
+{
+	/**
+	 * This constructor initializes the base class for SHA384 hashing.
+	 */
+	public SHA512()
+	{
+		super(DIGEST.SHA512);
+	}
+}
