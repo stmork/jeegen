6e71d5f56572091a2bbaf57c1ef0022da6220c33
Steffen A. Mork <linux-dev@morknet.de>
2014-09-19 05:37:03 +0000

    * Weiter Digest eingeführt:    o SHA-384    o SHA-512
    git-svn-id: svn+ssh://svn.code.sf.net/p/jeegen/code/trunk@586 2b804697-b555-4dd3-bbc5-4dc99f919620

---
+++ JEE6/de.itemis.jee6.util/src/main/java/de/itemis/jee6/util/SHA384.java
@@ -0,0 +1,15 @@
+package de.itemis.jee6.util;
+
+/**
+ * This class provides support methods for computing an SHA-384 hash.
+ */
+public class SHA384 extends AbstractMessageDigest
+{
+	/**
+	 * This constructor initializes the base class for SHA384 hashing.
+	 */
+	public SHA384()
+	{
+		super(DIGEST.SHA384);
+	}
+}
