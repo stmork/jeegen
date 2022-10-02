9778de7d81ddf56f775a4c84a5a06989793b069c
Steffen A. Mork <linux-dev@morknet.de>
2022-10-02 16:03:10 +0200

    Version bump.

--- make-release.sh
+++ make-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION_JEE6=1.2.7
-VERSION_JEE7=1.2.7
+VERSION_JEE6=1.2.8
+VERSION_JEE7=1.2.8
 
 for FILE in JEE?/org.jeegen.jee?/pom.xml JEE?/org.jeegen.jee?.ui/pom.xml JEE?/org.jeegen.jee?.feature/pom.xml
 do
